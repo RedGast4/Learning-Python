ce13ca77b9d61dd00949c6b0be111be93016da92
Данила Колбасов <kolbasov20023@gmail.com>
2024-11-11 15:01:02 +0300

    Обновление Module 2.3

--- module_2_3.py
+++ module_2_3.py
@@ -1,10 +1,14 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
-while index < 6:
+Data_Index = int(input('Введите индекс от |1 - 12| Ввод в консоль---> ')) - 1
+while index < Data_Index:
     number = my_list[index]
     index = index + 1
-    if number <= 0:
+    if number == 0:
         continue
+    elif number < 0:
+        print('ERROR: stop - number < 0', number )
+        break
     elif index == len(my_list):
         print(number)
     else:
